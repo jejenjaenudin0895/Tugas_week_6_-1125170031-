16a72fc1f74b91ebaad949bbc30e3e86be5f1a57
Jejen Jaenudin <1125170031@global.ac.id>
2025-11-03 18:34:50 +0700

    column posisi tengah

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -7,18 +7,20 @@ class MySplashScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
          body: //SingleChildScrollView(
-         Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Container(
-               height: 240,
-               width: 240,
-               decoration: const BoxDecoration(color: Colors.green,
-               shape: BoxShape.circle,
+         Center(
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Container(
+                 height: 240,
+                 width: 240,
+                 decoration: const BoxDecoration(color: Colors.green,
+                 shape: BoxShape.circle,
+                 ),
                ),
-             ),
-           ],
-          ),
+             ],
+            ),
+         ),
          );
 
        
