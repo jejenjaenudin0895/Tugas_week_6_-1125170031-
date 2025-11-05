8d2fcd4e67792488b28fc75f0b482107ea196ee0
Jejen Jaenudin <1125170031@global.ac.id>
2025-11-05 15:56:41 +0700

    membuat decoration password

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -10,55 +10,79 @@ class Login extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           Container(
-             height: 240,
-                  width: 240,
-                  decoration: const BoxDecoration(
-                    color: Colors.green,
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/test4.jpeg"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              SizedBox(height: 20),
-              Text("welcome",
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
+            Container(
+              height: 240,
+              width: 240,
+              decoration: const BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/test4.jpeg"),
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 2),
-              Text("Silahkan login untuk melanjutkan",
-                style: TextStyle(
-                  fontSize: 16.0, color: Colors.green,
-                ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "welcome",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 20),
-              Container(
-               margin: EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  keyboardType: 
-                  TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    hintText: "Masukan Email anda",
-                    prefixIcon: Icon(Icons.email_outlined),
-                    border : 
-                      OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color : Colors.green, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color : Colors.orange, width: 2),
-                      )
-                        
+            ),
+            SizedBox(height: 2),
+            Text(
+              "Silahkan login untuk melanjutkan",
+              style: TextStyle(fontSize: 16.0, color: Colors.green),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "Masukan Email anda",
+                  prefixIcon: Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.green, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.orange, width: 2),
                   ),
                 ),
-              )
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  hintText: "Masukan Password anda",
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                  suffixIcon: Icon(Icons.visibility_off_rounded),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.green, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.orange, width: 2),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
